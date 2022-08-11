608e7317d154efc9e57adb1c280388d309a993b1
louann123 <louisbabila@yahoo.com>
2022-08-10 21:24:20 -0500

    terraform-code

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region          = "us-east-1"
+ami             = "ami-0cff7528ff583bf9a"
+instance_type   = "t2.nano"
+keypair         = "classkey"
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0cff7528ff583bf9a"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.nano"
+}
+
+variable "keypair" {
+  type    = string
+  default = "classkey"
+}
+
+
+
+
+
+
+
